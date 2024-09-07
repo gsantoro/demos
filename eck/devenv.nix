282978b7764c24b28646ca48cde4fa3208158b92
gsantoro <giuseppe.santoro@elastic.co>
2024-09-07 16:18:22 +0100

    parametrized elastic stack with envsubst, bigger k3d cluster with agents, improved commands

--- eck/devenv.nix
+++ eck/devenv.nix
@@ -6,6 +6,8 @@
     pkgs.kubernetes-helm
     pkgs.go-task
     pkgs.curl
-    pkgs.k3d # latest in nix 5.4.4. latest v5.6.0 -> install manually if you want latest version from k8s.k3d.taskfile.yml at deps
+    pkgs.k3d
+    pkgs.kubectl
+    pkgs.kustomize
   ];
 }
