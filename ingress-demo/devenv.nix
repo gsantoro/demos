8dd7a2b8460920fa387f3b7c01bc521d4d89dedb
gsantoro <giuseppe.santoro@elastic.co>
2025-01-04 09:44:51 +0000

    ingress-demo project

---
+++ ingress-demo/devenv.nix
@@ -0,0 +1,24 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ 
+    pkgs.git
+
+    
+    # k8s
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+    pkgs.k3d  
+    pkgs.dive
+    pkgs.trivy
+    
+
+    # tools
+    pkgs.gum
+    pkgs.cookiecutter
+    pkgs.go-task
+  ];
+
+  
+}
